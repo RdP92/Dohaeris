f743cbb33469285d0bd634e4304f7983b5ed65cf
RdP92 <raphaeldepicciotto@gmail.com>
2015-10-26 11:13:56 +0100

    fast fertig

--- html/sendCommand.js
+++ html/sendCommand.js
@@ -2,11 +2,11 @@
 	function sendCommand( type, input, output) {
 	
 	  var xhttp = new XMLHttpRequest();
-	  xhttp.onreadystatechange = function() {
+	  /*xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
-		   (xhttp.responseText);
+		   alert(xhttp.responseText);
 		}
-	  }
+	  }*/
 	  xhttp.open("GET", "commander.php?type="+type+"&in="+input+"&out="+output, true);
 	  xhttp.send();
 	}
